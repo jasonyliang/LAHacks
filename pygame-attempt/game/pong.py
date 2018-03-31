82dfdfb52af9158a367f0192e4cd73b7dddce866
Jason Liang <jjjasonliang@gmail.com>
2018-03-31 12:53:09 -0700

    redo

--- pygame-attempt/game/pong.py
+++ pygame-attempt/game/pong.py
@@ -1,10 +1,8 @@
 import pygame
-
 from pygame.locals import *
-from sys import exit
+import sys
 import random
 
-pygame.init()
 
-screen=pygame.display.set_mode((640,480),0,32)
-pygame.display.set_caption("Pong Pong!")
+
+
